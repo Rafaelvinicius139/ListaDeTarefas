a04afa38473d57797cfc5aeeb5feccdfc6da84d9
Rafael Vinicius Dias dos Reis Coelho <rafaelvinicius139@gmail.com>
2025-01-12 19:33:20 -0300

    Update logando.php

--- logando.php
+++ logando.php
@@ -4,22 +4,11 @@
   }
 
   if(!isset($_SESSION['id'])){
-    die("<h1>voce nãp pode acessar esse sisteman por favor logar no sistema. <a href='\index.php\'>entrar</a></h1> ");
+    die("acesso negado <br>  <a href='index.php'>entrar</a>");
   }
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="paginas.css">
-  <title>Document</title>
-</head>
-<body>
-  
-</body>
-</html>
+
 
 
